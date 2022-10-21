e4037219b8a52e4be4253dca8f371eabe0ff48e4
Eduardo Ulises Martínez Vaca <81331013+EduardoMV@users.noreply.github.com>
2022-10-21 02:00:11 -0500

    NavBar

--- app.js
+++ app.js
@@ -11,7 +11,24 @@ app.set('views', __dirname);
 
 
 app.get("/",(req, res) =>{
+    // Acá iran los datos a guardar
+    const chunks = [];
+    // Llamamos a la API de GOT
+    const url = "https://anapioficeandfire.com/api/characters/583"
+    // Usamos un response que nos dirija al html principal
     res.sendFile(__dirname + "/public/html/index.html");
+    // Función en proceso...
+    https.get(url, (response)=>{
+        response.on("data",(data)=>{
+            chunks.push(data);
+        });
+    //     response.on("end",()=>{
+    //         actualId = 1;
+    //         let rendered = setVars(chunks);
+
+    //         res.render(__dirname + "/index.html", rendered);
+    //     });
+    });
 });
 
 app.listen(3000, (err) => {
