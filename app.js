3023a1f082056f8021518be8d566049b03754f8c
Eduardo Ulises Martínez Vaca <81331013+EduardoMV@users.noreply.github.com>
2022-10-21 15:10:36 -0500

    Base functionallity

--- app.js
+++ app.js
@@ -8,27 +8,102 @@ app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.set('views', __dirname);
 
-
+actualId = 0;
 
 app.get("/",(req, res) =>{
-    // Acá iran los datos a guardar
-    const chunks = [];
-    // Llamamos a la API de GOT
-    const url = "https://anapioficeandfire.com/api/characters/583"
-    // Usamos un response que nos dirija al html principal
-    res.sendFile(__dirname + "/public/html/index.html");
-    // Función en proceso...
-    https.get(url, (response)=>{
-        response.on("data",(data)=>{
-            chunks.push(data);
-        });
-    //     response.on("end",()=>{
-    //         actualId = 1;
-    //         let rendered = setVars(chunks);
 
-    //         res.render(__dirname + "/index.html", rendered);
-    //     });
-    });
+    var url = "https://thronesapi.com/api/v2/Characters/" + actualId;
+    https.get(url, (response) =>{
+        let data = "";
+        response
+            .on("data", (jdata)=> {
+                data += jdata;
+            })
+            .on("end", () => {
+                var jsonData = JSON.parse(data);
+                console.log(jsonData["firstName"])
+                res.render("public/html/index.html", { 
+                    ID: jsonData["id"],
+                    LastName: jsonData["lastName"], 
+                    FirstName: jsonData["firstName"],
+                    Title: jsonData["title"],
+                    Family: jsonData["family"],
+                    ImageURL: jsonData["imageUrl"]
+            });
+            })
+            .on("error", (e) => {
+                console.log("Error ${e.message}");
+                res.send("Error ${e.message}");
+            });
+        });
+});
+
+app.post("/next",(req, res)=>{
+    if(actualId==52){
+        actualId=0;
+    }else{
+        actualId++;
+    }
+
+    var url = "https://thronesapi.com/api/v2/Characters/" + actualId;
+    https.get(url, (response) =>{
+        let data = "";
+        response
+            .on("data", (jdata)=> {
+                data += jdata;
+            })
+            .on("end", () => {
+                var jsonData = JSON.parse(data);
+                console.log(jsonData["firstName"])
+                res.render("public/html/index.html", { 
+                    ID: jsonData["id"],
+                    LastName: jsonData["lastName"], 
+                    FirstName: jsonData["firstName"],
+                    Title: jsonData["title"],
+                    Family: jsonData["family"],
+                    ImageURL: jsonData["imageUrl"]
+                });
+
+            })
+            .on("error", (e) => {
+                console.log("Error ${e.message}");
+                res.send("Error ${e.message}");
+            })
+        });
+});
+
+app.post("/previous",(req, res)=>{
+    if(actualId==0){
+        actualId=52;
+    }else{
+        actualId--;
+    }
+
+    var url = "https://thronesapi.com/api/v2/Characters/" + actualId;
+    https.get(url, (response) =>{
+        let data = "";
+        response
+            .on("data", (jdata)=> {
+                data += jdata;
+            })
+            .on("end", () => {
+                var jsonData = JSON.parse(data);
+                console.log(jsonData["firstName"])
+                res.render("public/html/index.html", { 
+                    ID: jsonData["id"],
+                    LastName: jsonData["lastName"], 
+                    FirstName: jsonData["firstName"],
+                    Title: jsonData["title"],
+                    Family: jsonData["family"],
+                    ImageURL: jsonData["imageUrl"]
+                });
+
+            })
+            .on("error", (e) => {
+                console.log("Error ${e.message}");
+                res.send("Error ${e.message}");
+            })
+        });
 });
 
 app.listen(3000, (err) => {
